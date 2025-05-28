33da7f819780268ed25ce424ce4399e5c45c44c5
blajandrei04 <andrei.florin.blaj@stud.ubbcluj.ro>
2025-05-28 22:13:05 +0300

    Added yearly/weekly stats to the stats page and added the front for changingn profile pic

--- UI/src/app/profile/profile.component.ts
+++ UI/src/app/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, EventEmitter, Output } from '@angular/core';
+import { Component, inject, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,6 +9,7 @@ import { Router } from '@angular/router';
 })
 export class ProfileComponent {
   @Output() close = new EventEmitter<void>();
+  @ViewChild('fileInput') fileInput!: ElementRef;
 
   router = inject(Router);
 
@@ -16,8 +17,24 @@ export class ProfileComponent {
     name: 'Blaj Andrei',
     email: 'blajandrei04@gmail.com',
     phone: '0745989117',
-    location: 'Romania'
+    location: 'Romania',
+    avatar: 'assets/profilBlaj.jpeg'
   };
+
+  triggerFileInput() {
+    this.fileInput.nativeElement.click();
+  }
+
+  onFileSelected(event: Event) {
+    const file = (event.target as HTMLInputElement).files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.profile.avatar = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
 
   logout() {
     localStorage.removeItem('loggedInUser');
