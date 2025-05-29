6b1b9b69bf3fa4b07409edacbc2ecc45c239defb
Petru-Adelin <petru.o.adelin24@gmail.com>
2025-05-29 17:21:00 +0300

    Login/Sigin mechanism is up

--- Server/BActive/Login/urls.py
+++ Server/BActive/Login/urls.py
@@ -2,7 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.base, name='Login'),
+    path('login/', views.login, name='Login'),
+    path('signin/', views.signin, name='SingIn'),
     path('csrf/', views.aquire_csrf, name='aquire csrf'),
     path('filter/', views.filter_user),
     path('delete/', views.delete_user),
